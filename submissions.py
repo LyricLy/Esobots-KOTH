eda3915de1aba202da5d132ea959f97cc666ea78
Tyler L <tslimkemann42@gmail.com>
2018-05-23 01:59:36 -0700

    new boye

--- submissions.py
+++ submissions.py
@@ -25,4 +25,12 @@ def low_random(*args):
 def high_random(*args):
     return random.choice([1,2,2,3,3,3])
 
-players = [take_one, take_two, take_three, tit_for_tat, random_strategy, low_random, high_random]
+def minimum_high_random(turn, round, fish, results, state):
+    if round == 1:
+        return 1
+    elif round == 2:
+        return random.choice([1,2,2])
+    else:
+        return random.choice([1,2,2,3,3,3])
+
+players = [take_one, take_two, take_three, tit_for_tat, random_strategy, low_random, high_random, minimum_high_random]
